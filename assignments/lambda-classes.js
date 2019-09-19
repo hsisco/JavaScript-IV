7bc3ed46e9135b7331afebc81ddd6f8babdaf17e
hsisco <hysensisco@gmail.com>
2019-09-19 12:30:50 -0700

    Instructor constructed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,18 @@ class Person{
       return `Hello my name is ${this.name}, I am from ${this.location}.`
   }
 }
+
+class Instructor extends Person{
+  constructor(attributes){
+      super(attributes);
+      this.specialty = attributes.specialty;
+      this.favLanguage = attributes.favLanguage;
+      this.catchPhrase = attributes.catchPhrase
+  }
+  demo(){
+      return `Today we are learning about ${this.specialty}.`;
+  }
+  grade(student, subject){
+      return `${student.name} receives a perfect score on ${subject}.`
+  }
+}
